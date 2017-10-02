2dcf5c96dc2962c61787f48a1aba7f86b9d14203
Fossia <yxxy611@hotmail.com>
2017-10-02 13:03:55 +0800

    Add MenuView class for menu button in the left_bottom conner

---
+++ app/src/main/java/com/SiWei/PaintingApp/MenuView.java
@@ -0,0 +1,16 @@
+package com.SiWei.PaintingApp;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MenuView extends View implements View.OnClickListener {
+    public MenuView(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
